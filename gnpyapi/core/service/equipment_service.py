35df22712f037a25256431dad6809f386632a866
Renato Ambrosone <renato.ambrosone@polito.it>
2025-02-26 12:25:18 +0100

    refactor: API now rely on gnpy function without re-implementation

---
+++ gnpyapi/core/service/equipment_service.py
@@ -0,0 +1,15 @@
+# coding: utf-
+import json
+import os
+import uuid
+
+from injector import Inject
+
+from gnpyapi.core.exception.equipment_error import EquipmentError
+from gnpyapi.core.service import config_service
+
+
+class EquipmentService:
+
+    def __init__(self):
+        pass
---
+++ gnpyapi/core/service/topology_service.py
@@ -0,0 +1,16 @@
+# coding: utf-
+import json
+import os
+import uuid
+
+from gnpyapi.core.exception.topology_error import TopologyError
+from gnpyapi.core.service import config_service
+
+
+class TopologyService:
+    def __init__(self):
+        pass
+
+
+
+
