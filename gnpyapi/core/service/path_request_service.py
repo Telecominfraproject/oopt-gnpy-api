8b63efc3e94c083363ce86a5bc195f6aec28d7c2
Renato Ambrosone <renato.ambrosone@polito.it>
2025-03-11 14:18:40 +0100

    Error handling: added error catching when creating equipment in path request
    Change-Id: I2e4ee87293c491102522e142408577fd11f1a56e

--- gnpyapi/core/service/path_request_service.py
+++ gnpyapi/core/service/path_request_service.py
@@ -2,8 +2,10 @@
 
 import logging
 
-from gnpy.tools.worker_utils import designed_network, planning
+from gnpy.core.exceptions import EquipmentConfigError
 from gnpy.tools.json_io import results_to_json, load_eqpt_topo_from_json
+from gnpy.tools.worker_utils import designed_network, planning
+from gnpyapi.core.exception.equipment_error import EquipmentError
 
 _logger = logging.getLogger(__name__)
 
@@ -14,9 +16,15 @@ class PathRequestService:
         pass
     @staticmethod
     def path_request(topology: dict, equipment: dict, service: dict = None)->dict:
-        (equipment, network) = load_eqpt_topo_from_json(equipment, topology)
-        network, _, _ = designed_network(equipment, network)
-        # todo parse request
-        _, _, _, _, _, result =  planning(network, equipment, service)
-
-        return results_to_json(result)
+        try:
+            (equipment, network) = load_eqpt_topo_from_json(equipment, topology)
+            network, _, _ = designed_network(equipment, network)
+            # todo parse request
+            _, _, _, _, _, result = planning(network, equipment, service)
+            return results_to_json(result)
+        except EquipmentConfigError as e:
+            _logger.error(f"An equipment error occurred: {e}")
+            raise EquipmentError(str(e))
+        except Exception as e:
+            _logger.error(f"An error occurred during path request: {e}")
+            raise
