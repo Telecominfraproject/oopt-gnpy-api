e5c728230184052b0a5103b53ad28c308866bbfe
Renato Ambrosone <renato.ambrosone@polito.it>
2026-02-04 13:57:32 +0100

    fix: version endpoint and test now rely on common variable
    Change-Id: Ic78a7a25e83c33196fe9b73d82133e8375408338

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import pytest
 from flask_injector import FlaskInjector
 
-from gnpyapi.core import app
+from gnpyapi.core import app, API_VERSION
 from tests.utils.input import read_json_file
 
 
@@ -16,8 +16,6 @@ TEST_DATA_DIR = Path(__file__).parent / 'data'
 TEST_REQ_DIR_LEGACY = TEST_DATA_DIR / 'req' / 'legacy'
 TEST_REQ_DIR = TEST_DATA_DIR / 'req'
 TEST_RES_DIR = TEST_DATA_DIR / 'res'
-
-API_VERSION = '/api/v0.2'
 
 
 @pytest.fixture
@@ -49,4 +47,4 @@ def test_legacy_echo(client):
 def test_status(client):
     response = client.get(f"{API_VERSION}/status")
     assert response.status_code == 200
-    assert response.get_json() == {"version": "v0.2", "status": "ok"}
+    assert response.get_json() == {"version": f"{API_VERSION}", "status": "ok"}
