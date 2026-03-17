a9df26540e5a4b145a6cf4dbca16debbc7129893
Renato Ambrosone <renato.ambrosone@polito.it>
2026-03-17 15:20:06 -0700

    refactor(api): migrate from Flask to FastAPI
    Change-Id: I9ebfcd03ad81d3d2f0158e9c78c82b839af25e05

--- gnpyapi/core/exception/exception_handler.py
+++ gnpyapi/core/exception/exception_handler.py
@@ -1,34 +1,36 @@
 # coding: utf-8
-import json
+from http import HTTPStatus
 import re
 
-import werkzeug
+from starlette.exceptions import HTTPException
+from starlette.responses import JSONResponse
 
 from gnpyapi.core.model.error import Error
 
 _reaesc = re.compile(r'\x1b[^m]*m')
 
 
-def common_error_handler(exception):
+def common_error_handler(_request, exception):
     """
 
     :type exception: Exception
 
     """
     status_code = 500
-    if not isinstance(exception, werkzeug.exceptions.HTTPException):
-        exception = werkzeug.exceptions.InternalServerError()
-        exception.description = "Something went wrong on our side."
+    if not isinstance(exception, HTTPException):
+        message = HTTPStatus.INTERNAL_SERVER_ERROR.phrase
+        description = "Something went wrong on our side."
     else:
-        status_code = exception.code
-    response = Error(message=exception.name, description=exception.description,
-                     code=status_code)
+        status_code = exception.status_code
+        message = HTTPStatus(status_code).phrase
+        description = exception.detail
+    response = Error(message=message, description=description, code=status_code)
 
-    return werkzeug.Response(response=json.dumps(response.__dict__), status=status_code, mimetype='application/json')
+    return JSONResponse(response.__dict__, status_code=status_code)
 
 
-def bad_request_handler(exception):
+def bad_request_handler(_request, exception):
     exception_str = " ".join(str(exception).split())
     response = Error(message='bad request', description=_reaesc.sub('', exception_str.replace("\n", " ")),
                      code=400)
-    return werkzeug.Response(response=json.dumps(response.__dict__), status=400, mimetype='application/json')
+    return JSONResponse(response.__dict__, status_code=400)
